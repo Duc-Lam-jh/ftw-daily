20fb152faf44bfa7b32c7e15b97d502af6ce49b6
Duc-Lam-jh <duc.lam@journeyh.io>
2022-03-01 11:05:51 +0700

    remove default listing page components

--- src/containers/ListingPage/ListingPage.js
+++ src/containers/ListingPage/ListingPage.js
@@ -435,13 +435,6 @@ export class ListingPageComponent extends Component {
                     onContactUser={this.onContactUser}
                   />
                   <SectionDescriptionMaybe description={description} />
-                  <SectionFeaturesMaybe options={amenityOptions} publicData={publicData} />
-                  <SectionRulesMaybe publicData={publicData} />
-                  <SectionMapMaybe
-                    geolocation={geolocation}
-                    publicData={publicData}
-                    listingId={currentListing.id}
-                  />
                   <SectionReviews reviews={reviews} fetchReviewsError={fetchReviewsError} />
                   <SectionHostMaybe
                     title={title}
