971c0df37c1c925dfb3b6aaf122614dcb99e7736
Duc-Lam-jh <duc.lam@journeyh.io>
2022-03-01 15:18:47 +0700

    add function to toggle section subject and level

--- src/containers/ListingPage/SectionSubjectLevel.js
+++ src/containers/ListingPage/SectionSubjectLevel.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useRef, useState } from 'react';
 import { FormattedMessage } from '../../util/reactIntl';
 
 import css from './ListingPage.module.css';
 
 const SectionSubjectLevel = props => {
   const { options, publicData } = props;
+  const [isSectionHidden, setIsSectionHidden] = useState(false);
   if (!publicData || !publicData.subject || !publicData.level || !publicData.level.level) {
     return null;
   }
 
+  const handleToggleSection = () => {
+    setIsSectionHidden(!isSectionHidden);
+  }
+
+  const toggleButtonRef = useRef(null);
   const levelLabel = options.filter(item => item.key === publicData.level.level);
 
   return (
-    <div className={css.sectionFeatures}>
-      <h2 className={css.featuresTitle}>
+    <div className={css.sectionSubject}>
+        <div ref={toggleButtonRef} className={isSectionHidden ? classNames(css.toggleButton, css.hidden) : css.toggleButton} onClick={handleToggleSection}></div>
+      <h2 className={css.subjectTitle}>
         <FormattedMessage id="ListingPage.subjectTitle" />
       </h2>
-      <p>
+      <p className={isSectionHidden ? classNames(css.subjectAndLevel, css.hidden) : css.subjectAndLevel}>
         <FormattedMessage
           id="ListingPage.subjectAndLevel"
           values={{
