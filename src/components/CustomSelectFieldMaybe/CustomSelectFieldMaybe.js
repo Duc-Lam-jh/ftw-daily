6432d5c21c53ea9ca4d27bb9fabc2f421f89f35f
Duc-Lam-jh <duc.lam@journeyh.io>
2022-03-03 15:11:02 +0700

    add required props to custom select

--- src/components/CustomSelectFieldMaybe/CustomSelectFieldMaybe.js
+++ src/components/CustomSelectFieldMaybe/CustomSelectFieldMaybe.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { required } from '../../util/validators';
+import { required as requiredValidator } from '../../util/validators';
 import { FieldSelect } from '../index';
 
 import css from './CustomSelectFieldMaybe.module.css';
 
 const CustomSelectFieldMaybe = props => {
-  const { name, id, options, formName, intl, hidePlaceholder, disabled } = props;
+  const { name, id, options, formName, intl, hidePlaceholder, disabled, required } = props;
   const label = intl.formatMessage({
     id: `${formName}.${name}Label`,
   });
   const placeholder = intl.formatMessage({
     id: `${formName}.${name}Placeholder`,
   });
-  const requiredMessage = required(
+  const requiredMessage = required ? requiredValidator(
     intl.formatMessage({
       id: `${formName}.${name}Required`,
     })
-  );
+  ) : null;
   return options ? (
     <FieldSelect
       className={css.selectField}
